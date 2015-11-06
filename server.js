ab91401274404120436f002354cf9b803d898b2d
Mohamed Khelif <khelif96@gmail.com>
2015-11-06 16:32:02 -0500

    Got Message Sending up

--- server.js
+++ server.js
@@ -11,14 +11,22 @@ var newMessage = "";
 
 console.log("Server Started on port 3000")
 
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+  console.log('Server started on '+ add + ':3000');
+})
+
 io.sockets.on('connection', function(socket){
   console.log("Client Connected");
+
   socket.emit('message', {value : newMessage});
+
   socket.on('message', function(data){
     console.log("Message recieved " + data.value);
     newMessage = data.value;
-    var buf = new Buffer(5);
-    buf.writeUint8(newMessage,0);
+
+    var buf = new Buffer(1);
+    buf.writeUInt8(newMessage,0);
+
     io.sockets.emit('message', {value: newMessage});
 
   })
