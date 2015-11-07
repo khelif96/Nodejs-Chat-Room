8aa8b4f90eea0ef95e116ef3ad29ab51e4875b0b
khelif96 <khelif96@gmail.com>
2015-11-07 11:10:58 -0800

    Comments
    Commented some code

--- server.js
+++ server.js
@@ -2,36 +2,41 @@ express = require('express'); // Web Server
 app = express();
 server = require('http').createServer(app);
 io = require('socket.io').listen(server); // Web Socket Server
+var port = 3000;  // Desired port, Default is 3000
+server.listen(port); // Start the server on port, Default is 3000
+app.use(express.static('public')); // Folder for the static html pages
 
-server.listen(3000); // Start the server on port 3000
-app.use(express.static('public'));
-
-var messages = [];
+var messages = []; // Array to store previous messages
 var newMessage = "";
 
+// Function to get the local ip addresses
 require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-  console.log('Server started on '+ add + ':3000');
+  console.log('Server started on '+ add + ':' + port);
 })
 
+// On client connection
 io.sockets.on('connection', function(socket){
   console.log("Client Connected");
 
+// Send all previous messages
   for(i = 0; i<messages.length; i++){
     socket.emit('message', {value : messages[i]});
   }
 
+// When a message is recieved
   socket.on('message', function(data){
     newMessage = data.value;
-
     // var buf = new Buffer(1);
     // buf.writeUInt8(newMessage,0);
-    messages.push(newMessage);
-    io.sockets.emit('message', {value: newMessage});
+    messages.push(newMessage); // Add the message to the messages array
+    io.sockets.emit('message', {value: newMessage}); // Send the message to all the other clients
 
   })
+
+  // When a client disconnects
   socket.on("disconnect", function() {
     // request closed unexpectedly
-    console.log("CLient Disconnect");
+    console.log("Client Disconnect");
   });
 
 });
