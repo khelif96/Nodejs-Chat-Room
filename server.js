9ff0ae0ff8aff280bb2933c330e671d21a05f1d5
Mohamed Khelif <khelif96@gmail.com>
2015-11-08 20:53:54 -0500

    Now sends previous Usernames with messages

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ server.listen(port); // Start the server on port, Default is 3000
 app.use(express.static('public')); // Folder for the static html pages
 
 var messages = []; // Array to store previous messages
+var users = []; // Array to store previous users
 var newMessage = "";
 
 // Function to get the local ip addresses
@@ -20,7 +21,7 @@ io.sockets.on('connection', function(socket){
 
 // Send all previous messages
   for(i = 0; i<messages.length; i++){
-    socket.emit('message', {value : messages[i]});
+    socket.emit('message', {value : messages[i]}, {value: users[i]});
   }
 
 // When a message is recieved
@@ -32,6 +33,7 @@ io.sockets.on('connection', function(socket){
     // var buf = new Buffer(1);
     // buf.writeUInt8(newMessage,0);
     messages.push(newMessage); // Add the message to the messages array
+    users.push(newUserName);
     io.sockets.emit('message', {value: newMessage},{value: newUserName}); // Send the message to all the other clients
 
   })
